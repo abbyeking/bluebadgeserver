ed4a1a2cfd8dd7fc296d3ec1cd14d7d8e277403b
Jeremy Guler <bubulldog20@yahoo.com>
2021-02-20 11:41:06 -0500

    pushing so I can pull

--- app.js
+++ app.js
@@ -7,14 +7,13 @@ let recipe = require('./controllers/recipecontroller');
 let user = require('./controllers/usercontroller');
 
 sequelize.sync();
-//sequelize.sync({force: true})
+sequelize.sync({force: true})
 //above line will create and resets the database with the force: true
 //comment out to update database
 
 app.use(express.json());
 
 app.use('/user', user);
-
 
 app.use('/recipe', recipe);
 
