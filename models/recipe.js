9e5d661fd38561679fd3d27f58deaf75f53723e5
aligendza <73717510+aligendza@users.noreply.github.com>
2021-03-04 10:19:44 -0500

    previous version spacing change

--- models/recipe.js
+++ models/recipe.js
@@ -21,7 +21,7 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.INTEGER,
             allowNull: false
         }
-            // image: {
+        // image: {
         //     type: DataTypes.STRING,
         //     allowNull: true
         // },
