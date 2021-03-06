4df2ba430fe5bef4051448329e2c484fbc341fcd
Abby King <abbyking@Abbys-MacBook-Pro.local>
2021-03-06 11:45:09 -0500

    first commit

--- app.js
+++ app.js
@@ -20,6 +20,6 @@ app.use(express.json());
 app.use('/user', user);
 app.use('/recipe', recipe);
 
-app.listen(3000, () => {
-    console.log('App is running on port 3000')
+app.listen(process.env.PORT, () => {
+    console.log(`server is listening on port ${process.env.PORT}`)
 })
