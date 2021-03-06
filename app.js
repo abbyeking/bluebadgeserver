2ca62a23bdcc288a409db8346528a3cfe5fca6e8
abbyeking <73675464+abbyeking@users.noreply.github.com>
2021-03-06 10:45:31 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ let user = require('./controllers/usercontroller');
 
 sequelize.sync();
 // sequelize.sync({force: true})
-//above line will create and resets the database with the force: true
-//comment out to update database
 app.use(require("./middleware/headers"));
 
 app.options('*', (req, res) => {
@@ -22,13 +20,6 @@ app.use(express.json());
 app.use('/user', user);
 app.use('/recipe', recipe);
 
-// app.use('/test', (req, res) => {
-//     res.send('This is a server message')
-// })
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//     console.log(`App is running on ${PORT}`)
-// })
 app.listen(3000, () => {
     console.log('App is running on port 3000')
 })
