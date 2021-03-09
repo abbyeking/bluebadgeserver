cc8c3a7677b7194721c7a316f10865a025447279
Abby King <abbyking@Abbys-MacBook-Pro.local>
2021-03-08 20:19:02 -0500

    no ssl

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    dialectOptions: { ssl : {require: true, rejectUnauthorized: false}}
+    // dialectOptions: { ssl : {require: true, rejectUnauthorized: false}}
 });
 
 sequelize.authenticate().then(
